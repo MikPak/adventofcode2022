8601d63bde5915bdc0e567b2db44f11898f3196b
Mikko Pakkanen <mikkopak@gmail.com>
2022-12-10 20:50:57 +0200

    Day 3 pt 2

--- index.js
+++ index.js
@@ -154,15 +154,35 @@ const day3 = () => {
       if (ch === code) return isCapital(ch) ? 27 + i : i + 1
     }
   }
-  const values = []
-  contents.forEach((content) => {
-    const a = [...content.slice(0, content.length / 2)]
-    const b = [...content.slice(content.length / 2)]
-    const commonLetters = a.filter(e => b.includes(e)).slice(0, 1)
+  const grouped = []
+  let values = []
+  let groups = 0
+  contents.forEach((content, index) => {
+    values.push(content)
+    if((index + 1) % 3 === 0) {
+      grouped[groups] = [...values]
+      groups++
+      values = []
+    }
+  })
+  values = []
+  grouped.forEach((group, index) => {
+    const a = [...group[0]]
+    const b = [...group[1]]
+    const c = [...group[2]]
+    const commonLetters = a.filter(e => b.includes(e) && c.includes(e)).slice(0, 1)
+    console.log(commonLetters)
     commonLetters.forEach((letters) => {
       values.push(charValue(letters[0]))
     })
   })
+  /* const a = [...content.slice(0, content.length / 2)]
+  const b = [...content.slice(content.length / 2)]
+  const commonLetters = a.filter(e => b.includes(e)).slice(0, 1)
+  commonLetters.forEach((letters) => {
+    values.push(charValue(letters[0]))
+  }) */
+  console.log(values)
   const sum = values.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
   console.log(sum)
 }
